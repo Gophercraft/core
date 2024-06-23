dfa024c5c84e17d379c0500659c0bff781d696c2
Joshua Achorn <superp00t@tutanota.com>
2024-06-23 19:32:44 -0400

    fix(script): implement utility scripts

---
+++ script/install.bat
@@ -0,0 +1,2 @@
+CALL go install -v github.com/Gophercraft/core/cmd/gophercraft
+CALL go install -v github.com/Gophercraft/core/cmd/gophercraft_home
---
+++ script/installdeps.bat
@@ -0,0 +1,2 @@
+CALL go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+CALL go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
